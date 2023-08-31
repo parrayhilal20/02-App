95fd7ae2cf439aebd2ecbe4fe53f0fb312a5b5ad
parrayhilal20 <parayhilal13@gmail.com>
2023-08-31 13:08:23 +0530

    First Commit

---
+++ scratch-javascript.js
@@ -0,0 +1,52 @@
+'use strict';
+alert("Hello");
+[1,2].forEach(alert);
+
+// let message;
+// message = 'Hello!';
+// message = 'World!';
+// message = 'Javascript';
+
+// alert(message);
+
+// //let message;
+
+// let $ =  2;
+// let _ = 3;
+// alert($ + _)
+
+// alert(1 / 0);
+
+// alert(`The result is ${1+2}`);
+
+// let age;
+
+// let isBoss = confirm("Are you boss?");
+// alert(isBoss);
+
+// alert(age);
+
+// alert(`Type is - ${typeof 'stri'}`);
+
+// age = prompt('How to old are you?', 100);
+
+// alert(age);
+
+// alert(`You are ${age} years old!`);
+
+// alert(result);
+
+alert('2' > 7);
+
+alert(Boolean(1));
+alert(Boolean(0));
+
+alert(Boolean("Hello"));
+alert(Boolean(""));
+
+let counter = 1;
+a = counter++;
+alert(counter++)
+
+
+
